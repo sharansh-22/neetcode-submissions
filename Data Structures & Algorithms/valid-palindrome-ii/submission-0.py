e8c029f737d20f1525c6a86632c5e09f7b9085ac
Sharansh Pandey <sharansh.pandey.03415611923@adgips.ac.in>
2026-07-08 22:26:15 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def ispalindrome(l,r):
+            while l < r :
+                if s[l]!=s[r]:
+                    return False 
+                l+=1
+                r-=1
+            return True 
+        l=0
+        r=len(s)-1
+        while l < r :
+            if s[l]==s[r]:
+                l+=1
+                r-=1
+            else :
+                return( ispalindrome(l+1,r) or ispalindrome(l,r-1) ) 
+        return True 
